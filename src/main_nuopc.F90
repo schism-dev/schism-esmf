fc5c004bbb97bfef06c6e87093792866cb529c5f
Carsten Lemmen <carsten.lemmen@hzg.de>
2020-02-13 09:47:15 +0100

    Use AtmOcnProto from esmfcontrib as temmplate for nuopc app

---
+++ src/main_nuopc.F90
@@ -0,0 +1,76 @@
+! This code is part of the SCHISM-ESMF interface.  It defines
+! a main() program for a NUOPC coupled system of SCHISM with a
+! dummy atmosphere without a mediator.
+!
+! @copyright (C) 2020 Helmholtz-Zentrum Geesthacht
+! @author Carsten Lemmen carsten.lemmen@hzg.de
+!
+! @license under the Apache License, Version 2.0 (the "License");
+! you may not use this file except in compliance with the License.
+! You may obtain a copy of the License at
+!
+! 		http://www.apache.org/licenses/LICENSE-2.0
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
+!
+! This version accounts for halo(ghost) zone, because ESMF by default
+! partitions among nodes instead of elements
+
+#define ESMF_CONTEXT  line=__LINE__,file=ESMF_FILENAME,method=ESMF_METHOD
+#define ESMF_ERR_PASSTHRU msg="SCHISM subroutine call returned error"
+#undef ESMF_FILENAME
+#define ESMF_FILENAME "main_nuopc.F90"
+
+#define _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(X) if (ESMF_LogFoundError(rcToCheck=localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=X)) call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+#define _SCHISM_LOG_AND_FINALIZE_ON_ERRORS_(X) if (ESMF_LogFoundError(rcToCheck=localRc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=X) .or. ESMF_LogFoundError(rcToCheck=userRc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=X)) call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
+
+#define ESMF_METHOD "main"
+program main
+
+  use esmf
+  use nuopc
+
+  use toplevel, only: toplevelSetServices => SetServices
+
+  implicit none
+
+  integer                 :: localrc, userRc, rc
+  type(ESMF_GridComp)     :: topComp
+
+  ! Initialize ESMF
+  call ESMF_Initialize(logkindflag=ESMF_LOGKIND_MULTI, rc=localrc)
+  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
+
+  ! Create the top level component and register its services along with
+  ! profiling attributes
+  topComp = ESMF_GridCompCreate(name="esm", rc=localrc)
+  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
+
+  call ESMF_GridCompSetServices(topComp, toplevelSetServices, userRc=userRc, rc=localrc)
+  _SCHISM_LOG_AND_FINALIZE_ON_ERRORS_(rc)
+
+  call NUOPC_CompAttributeSet(topComp, name="Profiling", value="0", rc=localrc)
+  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
+
+  ! Initialize, run, and finalize top level
+  call ESMF_GridCompInitialize(topComp, userRc=userRc, rc=localrc)
+  _SCHISM_LOG_AND_FINALIZE_ON_ERRORS_(rc)
+
+  call ESMF_GridCompRun(topComp, userRc=userRc, rc=localrc)
+  _SCHISM_LOG_AND_FINALIZE_ON_ERRORS_(rc)
+
+  call ESMF_GridCompFinalize(topComp, userRc=userRc, rc=localrc)
+  _SCHISM_LOG_AND_FINALIZE_ON_ERRORS_(rc)
+
+  ! Clean up and finish
+  call ESMF_GridCompDestroy(topComp, rc=localrc)
+  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
+
+  call ESMF_Finalize()
+
+end program main
---
+++ src/toplevel_component.F90
@@ -0,0 +1,181 @@
+!==============================================================================
+! Earth System Modeling Framework
+! Copyright 2002-2019, University Corporation for Atmospheric Research,
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+! Laboratory, University of Michigan, National Centers for Environmental
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!==============================================================================
+
+module toplevel
+
+  !-----------------------------------------------------------------------------
+  ! Code that specializes generic ESM Component code.
+  !-----------------------------------------------------------------------------
+
+  use ESMF
+  use NUOPC
+  use NUOPC_Driver, &
+    driver_routine_SS             => SetServices, &
+    driver_label_SetModelServices => label_SetModelServices
+
+  use ATM, only: atmSS => SetServices
+  use OCN, only: ocnSS => SetServices
+
+  use NUOPC_Connector, only: cplSS => SetServices
+
+  implicit none
+
+  private
+
+  public SetServices
+
+  !-----------------------------------------------------------------------------
+  contains
+  !-----------------------------------------------------------------------------
+
+  subroutine SetServices(driver, rc)
+    type(ESMF_GridComp)  :: driver
+    integer, intent(out) :: rc
+
+    rc = ESMF_SUCCESS
+
+    ! NUOPC_Driver registers the generic methods
+    call NUOPC_CompDerive(driver, driver_routine_SS, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! attach specializing method(s)
+    call NUOPC_CompSpecialize(driver, specLabel=driver_label_SetModelServices, &
+      specRoutine=SetModelServices, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! set driver verbosity
+    call NUOPC_CompAttributeSet(driver, name="Verbosity", value="low", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+
+  !-----------------------------------------------------------------------------
+
+  subroutine SetModelServices(driver, rc)
+    type(ESMF_GridComp)  :: driver
+    integer, intent(out) :: rc
+
+    ! local variables
+    type(ESMF_Grid)               :: grid
+    type(ESMF_Field)              :: field
+    type(ESMF_Time)               :: startTime
+    type(ESMF_Time)               :: stopTime
+    type(ESMF_TimeInterval)       :: timeStep
+    type(ESMF_Clock)              :: internalClock
+    type(ESMF_GridComp)           :: child
+    type(ESMF_CplComp)            :: connector
+
+    rc = ESMF_SUCCESS
+
+    ! SetServices for ATM
+    call NUOPC_DriverAddComp(driver, "ATM", atmSS, comp=child, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompAttributeSet(child, name="Verbosity", value="low", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! SetServices for OCN
+    call NUOPC_DriverAddComp(driver, "OCN", ocnSS, comp=child, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompAttributeSet(child, name="Verbosity", value="low", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! Disabling the following macro, e.g. renaming to WITHCONNECTORS_disable,
+    ! will result in a driver that does not call connectors between the model
+    ! components. This mode can be used if all model components are driven
+    ! as independent models. However, even for independent models the
+    ! connectors can be set here, but will turn into no-ops.
+#define WITHCONNECTORS
+#ifdef WITHCONNECTORS
+    ! SetServices for atm2ocn
+    call NUOPC_DriverAddComp(driver, srcCompLabel="ATM", dstCompLabel="OCN", &
+      compSetServicesRoutine=cplSS, comp=connector, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompAttributeSet(connector, name="Verbosity", value="low", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    ! SetServices for ocn2atm
+    call NUOPC_DriverAddComp(driver, srcCompLabel="OCN", dstCompLabel="ATM", &
+      compSetServicesRoutine=cplSS, comp=connector, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompAttributeSet(connector, name="Verbosity", value="low", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+#endif
+
+    ! set the driver clock
+    call ESMF_TimeIntervalSet(timeStep, m=15, rc=rc) ! 15 minute steps
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_TimeSet(startTime, yy=2010, mm=6, dd=1, h=0, m=0, &
+      calkindflag=ESMF_CALKIND_GREGORIAN, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_TimeSet(stopTime, yy=2010, mm=6, dd=1, h=1, m=0, &
+      calkindflag=ESMF_CALKIND_GREGORIAN, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    internalClock = ESMF_ClockCreate(name="Application Clock", &
+      timeStep=timeStep, startTime=startTime, stopTime=stopTime, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_GridCompSet(driver, clock=internalClock, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+
+end module toplevel
