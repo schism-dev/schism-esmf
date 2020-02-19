cd8c7b4dbdf89241cec2f3df53e18f7088807943
Carsten Lemmen <carsten.lemmen@hzg.de>
2020-02-19 12:08:11 +0100

    Added new driver for uncoupled NUOPC schism

--- src/driver/driver_schism.F90
+++ src/driver/driver_schism.F90
@@ -1,6 +1,5 @@
 ! This code is part of the SCHISM-ESMF interface.  It defines
-! a driver for a NUOPC coupled system of SCHISM with a
-! dummy atmosphere without a mediator.
+! a driver for a NUOPC uncoupled system of SCHISM.
 !
 ! @copyright (C) 2020 Helmholtz-Zentrum Geesthacht
 ! @author Carsten Lemmen carsten.lemmen@hzg.de
@@ -21,7 +20,7 @@
 #define ESMF_CONTEXT  line=__LINE__,file=ESMF_FILENAME,method=ESMF_METHOD
 #define ESMF_ERR_PASSTHRU msg="SCHISM subroutine call returned error"
 #undef ESMF_FILENAME
-#define ESMF_FILENAME "driver_schism--atm.F90"
+#define ESMF_FILENAME "driver_schism.F90"
 
 #define _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(X) if (ESMF_LogFoundError(rcToCheck=localrc, ESMF_ERR_PASSTHRU, ESMF_CONTEXT, rcToReturn=X)) call ESMF_Finalize(rc=localrc, endflag=ESMF_END_ABORT)
 
@@ -38,10 +37,7 @@ module driver
     driver_routine_SS             => SetServices, &
     driver_label_SetModelServices => label_SetModelServices
 
-  use atmosphere, only: atmosphereSS => SetServices
   use schism, only: schismSS => SetServices
-
-  use NUOPC_Connector, only: cplSS => SetServices
 
   implicit none
 
@@ -87,39 +83,15 @@ subroutine SetModelServices(driver, rc)
   type(ESMF_TimeInterval)       :: timeStep
   type(ESMF_Clock)              :: internalClock
   type(ESMF_GridComp)           :: child
-  type(ESMF_CplComp)            :: connector
-  integer(ESMF_KIND_I4)         :: localrc, petCount, slotCount, i
+  integer(ESMF_KIND_I4)         :: localrc
   type(ESMF_Vm)                 :: vm
-  integer(ESMF_KIND_I4), allocatable :: petList(:)
   type(NUOPC_FreeFormat)        :: freeFormat
 
   rc = ESMF_SUCCESS
 
-  call ESMF_GridCompGet(driver, vm=vm, rc=localrc)
-  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-
-  call ESMF_VmGet(vm, petCount=petCount, rc=localrc)
-  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-
-  ! Use all but one pets in this petList for schism and the last
-  ! PET for the dummy atmosphere.  This runs the models concurrently.
-  ! For petCount=1, both run sequentially on the same PET
-  allocate(petList(max(1, petCount-1)))
-  do i=1, max(1, petCount-1)
-    petList(i) = i-1
-  enddo
-
-  ! SetServices for dummy atmosphereosphere and for schism ocean
   ! NUOPC_DriverAddGridComp(driver, compLabel, &
   !   compSetServicesRoutine, compSetVMRoutine, petList, info, driver, rc)
-  call NUOPC_DriverAddComp(driver=driver, compLabel="atmosphere", &
-    compSetServicesroutine=atmosphereSS, petList=(/petCount-1/), comp=child, rc=localrc)
-  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-
-  call NUOPC_CompAttributeSet(child, name="Verbosity", value="low", rc=localrc)
-  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-
-  call NUOPC_DriverAddComp(driver, "schism", schismSS, petList=petList, &
+  call NUOPC_DriverAddComp(driver, "schism", schismSS, &
     comp=child, rc=localrc)
   _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
 
@@ -133,27 +105,6 @@ subroutine SetModelServices(driver, rc)
   !call NUOPC_FreeFormatLog(freeFormat, rc=localrc)
   _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
 
-  ! SetServices for connectors atmosphere--ocean and vice versa
-  call NUOPC_DriverAddComp(driver, srcCompLabel="atmosphere",   dstCompLabel="schism", &
-    compSetServicesRoutine=cplSS, comp=connector, rc=localrc)
-  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-
-  call NUOPC_CompAttributeSet(connector, name="Verbosity", value="high", rc=localrc)
-  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-
-  ! Let's skipt the feedback for now
-  ! call NUOPC_DriverAddComp(driver, srcCompLabel="schism", dstCompLabel="atmosphere", &
-  !   compSetServicesRoutine=cplSS, comp=connector, rc=localrc)
-  ! _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-  !
-  ! call NUOPC_CompAttributeSet(connector, name="Verbosity", value="high", rc=localrc)
-  ! _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-
-  deallocate(petList)
-
-  ! call NUOPC_DriverGet(driver, slotCount=slotCount, rc=localrc)
-  _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
-
   call NUOPC_DriverEgestRunSequence(driver, freeFormat=freeFormat, rc=localrc)
   _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
 
@@ -164,9 +115,5 @@ subroutine SetModelServices(driver, rc)
   _SCHISM_LOG_AND_FINALIZE_ON_ERROR_(rc)
 
 end subroutine
-
-!NUOPC_StateWrite(state, fieldNameList, fileNamePrefix, overwrite, &
-!     status, timeslice, relaxedflag, rc)
-
 
 end module driver
