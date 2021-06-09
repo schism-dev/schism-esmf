58a9e2a891cf7a2df4eb6c8c9c77497132afb1c5
Carsten Lemmen <carsten.lemmen@hereon.de>
2021-06-09 21:48:57 +0200

    Added dual license statement to keep compatibility to mossco

--- src/schism/schism_cmi_nuopc.mk
+++ src/schism/schism_cmi_nuopc.mk
@@ -5,9 +5,11 @@
 #
 # @author Carsten Lemmen carsten.lemmen@hereon.de
 #
-# @license Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# @license dual licensed under the Apache License, Version 2.0
+# @license and the Gnu Public License version 3 or later
+#
+# you may not use this file except in compliance with the licenses.
+# You may obtain a copy of the Apache License at
 #
 # 		http://www.apache.org/licenses/LICENSE-2.0
 #
